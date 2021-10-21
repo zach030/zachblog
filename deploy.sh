a0ff227dcb927e0a54aace9b059f971dea6555dd
zach030 <946778530@qq.com>
2021-10-22 00:05:23 +0800

    newpost

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
+echo "Hugo Bot is Auto-push and deploy for you......"
 hugo -D
-git add . && git commit -m $0
-git push origin $1
+git add . && git commit -m $1
+git push origin $2
 scp -r -i "~/.ssh/new_wsl.pem" ./public/* root@81.69.255.174:/root/hugo/public
