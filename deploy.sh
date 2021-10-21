b9fddf5709d1aa9cff3ce814cb25a2b279fbfd21
zach030 <946778530@qq.com>
2021-10-22 00:03:01 +0800

    ./deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 hugo -D
-git add . && git commit -a 'new posts'
-git push origin master
+git add . && git commit -m $0
+git push origin $1
 scp -r -i "~/.ssh/new_wsl.pem" ./public/* root@81.69.255.174:/root/hugo/public
