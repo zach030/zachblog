396e0cc805b8b25f2c2bfd302720f528686cac91
zach <zachchou016@gmail.com>
2021-12-23 22:51:23 +0800

    add

---
+++ content/post/tinykv.md
@@ -0,0 +1,9 @@
++++
+author = "zach"
+title = "TinyKV Project-1"
+date = "2021-11-15"
+description = "A brief description of Hugo Shortcodes"
+tags = [
+    "tinykv",
+]
++++
--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,4 @@ echo "Hugo Bot is Auto-push and deploy for you......"
 hugo -D
 git add . && git commit -m $1
 git push origin $2
-scp -r -i "~/.ssh/new_wsl.pem" ./public/* root@81.69.255.174:/root/hugo/public
+scp -r -i "~/.ssh/mba.cer" ./public/* root@81.69.255.174:/root/hugo/public
