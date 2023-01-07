96910fd71f55b427d0efadbaac22f1a8aba7cfce
zach <zachchou016@gmail.com>
2023-01-07 18:35:32 +0800

    dapr-blog

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,4 @@ echo "Hugo Bot is Auto-push and deploy for you......"
 hugo -D
 git add . && git commit -m $1
 git push origin master
-scp -r -i "~/.ssh/mba.cer" ./public/* root@81.69.255.174:/root/hugo/public
+# scp -r -i "~/.ssh/mba.cer" ./public/* root@81.69.255.174:/root/hugo/public
